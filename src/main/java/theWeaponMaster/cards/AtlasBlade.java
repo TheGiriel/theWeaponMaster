a8e04eb136c29c153781ce418beef197890eafc4
Mersid <mersid.hazbiu@students.ffhs.ch>
2019-06-09 03:53:52 +0200

    Initial commit

---
+++ src/main/java/theWeaponMaster/cards/AtlasBlade.java
@@ -0,0 +1,55 @@
+package theWeaponMaster.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theWeaponMaster.DefaultMod;
+import theWeaponMaster.characters.TheDefault;
+
+import static theWeaponMaster.DefaultMod.makeCardPath;
+
+public class AtlasBlade extends AbstractRevolvers {
+
+    public static final String ID = DefaultMod.makeID(AtlasBlade.class.getSimpleName());
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+
+    public static final String IMG = makeCardPath("Attack.png");
+
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+
+    private static final CardRarity RARITY = CardRarity.RARE;
+    private static final CardTarget TARGET = CardTarget.ENEMY;
+    private static final CardType TYPE = CardType.ATTACK;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 3;
+    private static final int DAMAGE = 20;
+
+    public AtlasBlade() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+
+        upgraded = true;
+        baseDamage = DAMAGE;
+    }
+
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m){
+
+            AbstractDungeon.actionManager.addToBottom(
+                    new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+
+    }
+
+    @Override
+    public void upgrade(){
+
+    }
+
+}
