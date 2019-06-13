60dd495dd30bff95cbd3a69882c3be500e3f2a57
TheGiriel <mersid.hazbiu@students.ffhs.ch>
2019-06-13 19:18:49 +0200

    Berserk Works

--- src/main/java/theWeaponMaster/powers/ViciousPower.java
+++ src/main/java/theWeaponMaster/powers/ViciousPower.java
@@ -2,16 +2,11 @@ package theWeaponMaster.powers;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.utility.UseCardAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
-import com.megacrit.cardcrawl.powers.DexterityPower;
 import theWeaponMaster.DefaultMod;
 import theWeaponMaster.util.TextureLoader;
 
@@ -44,13 +39,15 @@ public class ViciousPower extends AbstractPower {
 
     @Override
     public void updateDescription() {
-        description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1] + amount + DESCRIPTIONS[2];
+        if (this.amount > 9) {
+            description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1] + amount + DESCRIPTIONS[3];
+        } else {
+            description = DESCRIPTIONS[0] + amount + DESCRIPTIONS[1] + amount + DESCRIPTIONS[2];
+        }
     }
 
     public float atDamageGive(float damage, DamageInfo.DamageType type) {
-
         return damage + this.amount;
-
     }
 
     public float atDamageReceive(float damage, DamageInfo.DamageType damageType) {
