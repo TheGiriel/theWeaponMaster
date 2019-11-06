66995ab0dd3866a90d4ea2a3d960a64266f9f2b3
TheGiriel <mersid.hazbiu@students.ffhs.ch>
2019-11-06 19:01:11 +0100

    Weapon Swapping 0.9: Now actually purges cards. Next up, for real: Make this a relic thing, then save the newest selected weapon and prevent picking it again.

--- src/main/java/theWeaponMaster/actions/ExchangeWeaponsAction.java
+++ src/main/java/theWeaponMaster/actions/ExchangeWeaponsAction.java
@@ -4,7 +4,6 @@ import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.powers.AbstractPower;
 import theWeaponMaster.powers.*;
 
 public class ExchangeWeaponsAction extends AbstractGameAction {
